2db8cf8edbf53214309cc0a01b1daa197ab2e650
masa44 <mashumalow@gmail.com>
2014-01-11 16:04:21 +0900

    create calc_string.pl

---
+++ calc_string.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use DDP;
+
+p calc_string($ARGV[0]);
+
+sub calc_string {
+    my $string = shift;
+    my $err_flg = 0;
+    my @calc_argv = ();
+
+    return undef if !$string;
+
+    if($string =~ /(-?\d*) (\+|\-|\*|\/|\%) (-?\d*)/){
+        return ($1 + $3) if $2 eq '+';
+        return ($1 - $3) if($2 eq '-');
+        return ($1 * $3) if($2 eq '*');
+        return ($1 / $3) if($2 eq '/');
+        return ($1 % $3) if($2 eq '%');
+    }
+}
+
